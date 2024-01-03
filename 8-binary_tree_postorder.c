d02280a33dc25bdd5ed49968e60b12f6be10d93c
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-03 15:34:00 +0100

    chnages 2

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,17 +5,12 @@
  * @tree: Pointer to the root node of the binary tree
  * @func: Pointer to a function to be applied to each node's data
  *
- * This function traverses a binary tree in a preorder manner,
- * applying the provided
- * function to the data of each visited node.
- * The traversal starts from the root
- * and goes recursively to the left subtree
- * before the right subtree.
+ * This function traverses a binary tree in a preorder manner, applying the provided
+ * function to the data of each visited node. The traversal starts from the root
+ * and goes recursively to the left subtree before the right subtree.
  *
- * @tree: Pointer to the root node of the binary tree.
- * If the tree is NULL, the function does nothing.
- * @func: Pointer to a function taking an
- *  parameter and returning void.
+ * @tree: Pointer to the root node of the binary tree. If the tree is NULL, the function does nothing.
+ * @func: Pointer to a function taking an int parameter and returning void.
  *        This function is applied to the data of each visited node during the traversal.
  *        If func is NULL, the function does nothing.
  */
@@ -25,6 +20,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
     if (tree == NULL)
         return;
 
+    
     binary_tree_postorder(tree->left, func);
     binary_tree_postorder(tree->right, func);
     func(tree->n);
