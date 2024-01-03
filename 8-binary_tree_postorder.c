3c84021ea84798259b1ad6f0d0e47394fbadd26b
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-03 15:33:20 +0100

    changes

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -22,8 +22,8 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-if (tree == NULL)
-    return;
+    if (tree == NULL)
+        return;
 
     binary_tree_postorder(tree->left, func);
     binary_tree_postorder(tree->right, func);
