437d9eb206fb3f393a2c7b95f7c31c50d0f0ebb0
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-05 21:17:59 +0100

    changes to get the betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,7 +2,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Inserts a node as the right child of another node
+ * binary_tree_insert_left - Inserts a node as the left child of another node
  * @parent: Pointer to the parent node
  * @value: Value to put in the new node
  *
@@ -10,31 +10,35 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return (NULL);
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+if (parent == NULL)
+{
+return (NULL);
+}
+new_node = malloc(sizeof(binary_tree_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
 
-    if (parent->left == NULL)
-    {
-        parent->left = new_node;
-    }
-    else
-    {
-        new_node->left = parent->left;
-        parent->left->parent = new_node;
-        parent->left = new_node;
-    }
+if (parent->left == NULL)
+{
+parent->left = new_node;
+}
+else
+{
+new_node->left = parent->left;
+parent->left->parent = new_node;
+parent->left = new_node;
+}
 
-    return (new_node);
+return (new_node);
 }
 
 
